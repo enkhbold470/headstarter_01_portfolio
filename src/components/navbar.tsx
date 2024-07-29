c21fb71bead12e0e0c95e359b6bb886c385f9eab
enkhbold470 <enkhbold470@gmail.com>
2024-07-29 11:16:39 +0000

    initial

---
+++ src/components/navbar.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+export default function Navbar(){
+    return (
+        <div className="flex justify-center">
+            <section id="#">
+                
+            </section>
+        </div>
+    )
+}
