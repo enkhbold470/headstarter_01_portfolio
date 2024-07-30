86e5eceb7edf114f614d19e4e2d42cb9e19ecef0
Enkhbold Ganbold <53986637+enkhbold470@users.noreply.github.com>
2024-07-30 03:18:20 -0700

    License

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,13 +3,13 @@ export default function Footer() {
     <footer className="flex justify-center gap-4 items-center py-2 m-0 bg-gray-200">
       <div className="flex gap-5">
         <a
-          href="/headstarter_01_portfolio/"
+          href="https://enkhbold470.github.io/headstarter_01_portfolio/"
           className="text-sm text-primary no-underline hover:underline"
         >
           About
         </a>
         <a
-          href="/headstarter_01_portfolio/projects"
+          href="https://enkhbold470.github.io/headstarter_01_portfolio/projects"
           className="text-sm text-primary no-underline hover:underline"
         >
           Projects
@@ -30,7 +30,7 @@ export default function Footer() {
         </a>
 
         <a
-          href="/headstarter_01_portfolio/license"
+          href="https://enkhbold470.github.io/headstarter_01_portfolio/license"
           className="text-sm text-primary "
         >
           © MIT License{" "}
